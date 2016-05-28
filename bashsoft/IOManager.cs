27a65f79ace66212401ce92504761829979aac38
Doncho Patronski <donstz@yahoo.com>
2016-05-28 12:04:58 +0300

    lab exercise

---
+++ bashsoft/IOManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BashSoft
+{
+    public static class IOManager
+    {
+
+        public static void TraverseDirectory(int depth)
+        {
+            OutputWriter.WriteEmptyLine();
+            int initialIdentation = SesionData.currentPath.Split('\\').Length;
+            Queue<string> subFolders = new Queue<string>();
+            subFolders.Enqueue(SesionData.currentPath);
+
+            while (subFolders.Count != 0)
+            {
+                string currentPath = subFolders.Dequeue();
+                int identation = currentPath.Split('\\').Length - initialIdentation;
+                if (depth - identation < 0)
+                {
+                    break;
+                }
+
+                foreach (var directoryPath in Directory.GetDirectories(currentPath))
+                {
+                    subFolders.Enqueue(directoryPath);
+                }
+                OutputWriter.WriteMessageOnNewLine(string.Format("{0}{1}", new string('-', identation), currentPath));
+
+                foreach (var file in Directory.GetFiles(currentPath))
+                {
+                    int indexOfLastSlash = file.LastIndexOf("\\");
+                    string fileName = file.Substring(indexOfLastSlash);
+                    OutputWriter.WriteMessageOnNewLine(new string('-', indexOfLastSlash) + fileName);
+                }
+            }
+        }
+
+        public static void CreateDirectoryInCurrentFolder(string name)
+        {
+            string path = Directory.GetCurrentDirectory() + "\\" + name;
+            Directory.CreateDirectory(path);
+        }
+
+        public static void ChangeCurrentDirectoryRelative(string relativePath)
+        {
+            if (relativePath == "..")
+            {
+                string currentPath = SesionData.currentPath;
+                int indexOfLastSlash = currentPath.LastIndexOf("\\");
+                string newPath = currentPath.Substring(0, indexOfLastSlash);
+                SesionData.currentPath = newPath;
+            }
+            else
+            {
+                string currentPath = SesionData.currentPath;
+                currentPath += "\\" + relativePath;
+                ChangeCurrentDirectoryAbsolute(currentPath);
+            }
+        }
+
+        private static void ChangeCurrentDirectoryAbsolute(string absolutePath)
+        {
+            if (!Directory.Exists(absolutePath))
+            {
+                OutputWriter.DisplayException(ExceptionMessages.InvalidPath);
+                return;
+            }
+
+            SesionData.currentPath = absolutePath;
+        }
+    }
+}
---
+++ bashsoft/Launcher.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BashSoft
+{
+    class Launcher
+    {
+        static void Main(string[] args)
+        {
+            ////IOManager.TraverseDirectory(@"D:\Torrents");
+            //Data.InitializeData();
+            ////Data.GetAllStudentsFromCourse("Unity");
+            //Data.GetStudentScoresFromCourse("Unity", "Ivan");
+
+            Tester.CompareContent(@"d\test1.txt", @"d\test2.txt");
+
+            //IOManager.CreateDirectoryInCurrentFolder("pesho");
+            //IOManager.TraverseDirectory(0);
+
+            string inputPath = @"C:\Users\DonchoP\Documents\Visual Studio 2015\
+                            Projects\AdvancedCSharp\BashSoft\BashSoft\bin\Debug\input";
+            string myOutputPath = @"C:\Users\DonchoP\Documents\Visual Studio 2015\
+                            Projects\AdvancedCSharp\BashSoft\BashSoft\bin\Debug\myOutput";
+            string expectedOutputPath = @"C:\Users\DonchoP\Documents\Visual Studio 2015\
+                            Projects\AdvancedCSharp\BashSoft\BashSoft\bin\Debug\expectedOutput";
+        }
+    }
+}
